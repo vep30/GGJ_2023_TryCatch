b3f4f6b962b044d183e89fb4a6e57567c13aadb5
Omni_k <kostya.300397@gmail.com>
2023-02-04 17:44:02 +0500

    no message

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -1,16 +1,26 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Point : MonoBehaviour
 {
     [SerializeField] private int weight;
-    [SerializeField] private Button _button;
-    [SerializeField] private Text text;
+    [SerializeField] private GameObject foodButton, waterButton, happinessButton;
     private bool isPositive;
 
     public void RandomWeight()
     {
         var rnd = Random.Range(100, 400);
-        weight = Mathf.RoundToInt(rnd/100)*10;
+        weight = Mathf.RoundToInt(rnd / 100) * 10;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Click to main point");
+        foodButton.SetActive(true);
+        waterButton.SetActive(true);
+        happinessButton.SetActive(true);
     }
 }
