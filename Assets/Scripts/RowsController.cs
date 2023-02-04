e423dafd7238402e65da7fe9e0c56f80961e4588
Omni_k <kostya.300397@gmail.com>
2023-02-04 21:31:11 +0500

    Добавил рандом весов точкам

--- Assets/Scripts/RowsController.cs
+++ Assets/Scripts/RowsController.cs
@@ -1,31 +1,83 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class RowsController : MonoBehaviour
     {
-        [SerializeField] private PointsController pointsController;
+        [SerializeField] private PointsController pointsController1,
+            pointsController2,
+            pointsController3,
+            pointsController4,
+            pointsController5;
 
         private int _positivePoints = 9, _negativePoints = 9, neutralPoints = 2;
-        
+
         public void InitPoints()
+        {
+            StartCoroutine(InitPointsCor());
+        }
+
+        private IEnumerator InitPointsCor()
+        {
+            yield return InitPointsNonNeutral(pointsController1);
+            yield return InitPointsNonNeutral(pointsController2);
+            yield return InitPointsNonNeutral(pointsController3);
+            yield return InitPointsWithNeutral(pointsController4);
+            yield return InitPointsNonNeutral(pointsController5);
+        }
+
+        private IEnumerator InitPointsNonNeutral(PointsController pointsController)
         {
             while (pointsController.haveFreeItem)
             {
                 var rnd = Random.Range(0, 91);
-                if (rnd > 0 && rnd <= 30 && neutralPoints != 0)
-                {
-                    pointsController.RandomPoint(PointsController.TypePoint.Neutral);
-                }
-                else if (rnd > 30 && rnd <= 60 && _negativePoints != 0)
+                if (rnd > 0 && rnd <= 45 && _negativePoints > 0)
                 {
                     pointsController.RandomPoint(PointsController.TypePoint.Negative);
+                    _negativePoints--;
+                    Debug.Log("Set Negative point");
                 }
-                else if (rnd > 60 && _positivePoints != 0)
+                else if (rnd>45 && _positivePoints > 0)
                 {
                     pointsController.RandomPoint(PointsController.TypePoint.Positive);
+                    _positivePoints--;
+                    Debug.Log("Set Positive point");
                 }
             }
+
+            return null;
+        }
+        private IEnumerator InitPointsWithNeutral(PointsController pointsController)
+        {
+            while (pointsController.haveFreeItem)
+            {
+                for (int i = 0; i < neutralPoints; i++)
+                {
+                    pointsController.RandomPoint(PointsController.TypePoint.Neutral);
+                    neutralPoints--;
+                    Debug.Log("Set Neutral point");
+
+                }
+                var rnd = Random.Range(0, 91);
+                if (rnd > 0 && rnd <= 45 && _negativePoints > 0)
+                {
+                    pointsController.RandomPoint(PointsController.TypePoint.Negative);
+                    _negativePoints--;
+                    Debug.Log("Set Negative point");
+
+                }
+                else if (rnd > 45 && _positivePoints > 0)
+                {
+                    pointsController.RandomPoint(PointsController.TypePoint.Positive);
+                    _positivePoints--;
+                    Debug.Log("Set Positive point");
+
+                }
+            }
+
+            return null;
         }
     }
 }
