9c9d6bf233ad61657322ca2788585254fba451cb
Evgeniy <vep3@inbox.ru>
2023-02-04 15:44:33 +0500

    Made a menu layout.

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
