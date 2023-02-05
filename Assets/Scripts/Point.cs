a3607b3a11a02a6eee2f27817db82de2b96d53f9
Omni_k <kostya.300397@gmail.com>
2023-02-05 11:13:55 +0500

    Добавлена ещё часть визуала

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -8,10 +8,17 @@ public class Point : MonoBehaviour
     [SerializeField] private ItemChoice foodButton, waterButton, happinessButton;
     [SerializeField] private SpriteRenderer sprite;
     [SerializeField] private SpriteRenderer food, water, happiness;
+    [SerializeField] private SpriteRenderer dotPoint;
 
     [SerializeField]
     private List<Sprite> spiteMinus10, spiteMinus20, spiteMinus30, spitePlus10, spitePlus20, spitePlus30;
 
+    [SerializeField] private Sprite dotPointPlus10,
+        dotPointPlus20,
+        dotPointPlus30,
+        dotPointMinus10,
+        dotPointMinus20,
+        dotPointMinus30;
     public int positionOnRow;
     public int numberRow;
     public bool isActive;
@@ -37,26 +44,32 @@ public class Point : MonoBehaviour
             case -30:
                 food.sprite = spiteMinus30[0];
                 water.sprite = spiteMinus30[1];
+                dotPoint.sprite = dotPointMinus30;
                 break;
             case -20:
                 food.sprite = spiteMinus20[0];
                 water.sprite = spiteMinus20[1];
+                dotPoint.sprite = dotPointMinus20;
                 break;
             case -10:
                 food.sprite = spiteMinus10[0];
                 water.sprite = spiteMinus10[1];
+                dotPoint.sprite = dotPointMinus10;
                 break;
             case 10:
                 food.sprite = spitePlus10[0];
                 water.sprite = spitePlus10[1];
+                dotPoint.sprite = dotPointPlus10;
                 break;
             case 20:
                 food.sprite = spitePlus20[0];
                 water.sprite = spitePlus20[1];
+                dotPoint.sprite = dotPointPlus20;
                 break;
             case 30:
-                food.sprite = spitePlus20[0];
-                water.sprite = spitePlus20[1];
+                food.sprite = spitePlus30[0];
+                water.sprite = spitePlus30[1];
+                dotPoint.sprite = dotPointPlus30;
                 break;
         }
         isActive = true;
